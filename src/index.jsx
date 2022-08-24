d0fe15c27dd4dc448bf0d8a96217f74d11dc055d
MirobidovM <miriliyosmiriobidov@gmail.com>
2022-08-24 21:18:23 +0500

    initial commit

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,6 @@ import { App } from './App';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(<App />);
